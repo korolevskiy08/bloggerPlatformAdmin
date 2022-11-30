78a5bf72fd729df94914b3c33d0d502a9097d002
korolevskiy08 <emkorolev12@gmail.com>
2022-11-30 12:03:55 +0300

    feat: add title superadmin

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -8,7 +8,10 @@ export const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={style.container}>
-        <p className={styles.title}>Blogger Platform</p>
+        <div className={styles.content}>
+          <p className={styles.title}>Blogger Platform</p>
+          <p>Superadmin</p>
+        </div>
       </div>
     </header>
   );
